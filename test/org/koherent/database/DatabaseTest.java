bee75430147a2e925ee51aee32c90f951820c52b
koher <koher@koherent.org>
2015-11-21 21:45:17 +0900

    Add `testUpdate` for `DatabaseTest`

--- test/org/koherent/database/DatabaseTest.java
+++ test/org/koherent/database/DatabaseTest.java
@@ -1,7 +1,7 @@
 package org.koherent.database;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
+import static org.junit.Assert.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -14,6 +14,8 @@ public abstract class DatabaseTest<I, V extends Value<I>, D extends Database<I, 
 	protected abstract D getDatabase();
 
 	protected abstract V createValue();
+
+	protected abstract V createValue(I id);
 
 	@Before
 	public void setUp() {
@@ -75,6 +77,110 @@ public abstract class DatabaseTest<I, V extends Value<I>, D extends Database<I, 
 			}
 			assertEquals(0, idToValue.size());
 		} catch (DatabaseException e) {
+			e.printStackTrace();
+			fail(e.getMessage());
+		}
+	}
+
+	@Test
+	public void testUpdate() {
+		D database = getDatabase();
+
+		final V originalValue = createValue();
+		final I id = originalValue.getId();
+		final V newValue = createValue(originalValue.getId());
+
+		try { // null to null -> nothing
+			V updated = database.update(id, new Database.Updater<V>() {
+				@Override
+				public V update(V value) {
+					assertNull(value);
+					return null;
+				}
+			});
+			assertNull(updated);
+		} catch (IllegalUpdateException | DatabaseException e) {
+			e.printStackTrace();
+			fail(e.getMessage());
+		}
+
+		try { // null to value with a wrong id
+			V updated = database.update(id, new Database.Updater<V>() {
+				@Override
+				public V update(V value) {
+					assertNull(value);
+					return createValue();
+				}
+			});
+			fail("Different id from the original: before = " + id
+					+ ", after = " + updated.getId());
+		} catch (DatabaseException e) {
+			e.printStackTrace();
+			fail(e.getMessage());
+		} catch (IllegalUpdateException e) {
+			// OK
+		}
+
+		try { // null to originalValue -> put
+			V updated = database.update(id, new Database.Updater<V>() {
+				@Override
+				public V update(V value) {
+					assertNull(value);
+					return originalValue;
+				}
+			});
+			assertValuesEqual(originalValue, updated);
+			assertValuesEqual(originalValue, database.get(id));
+		} catch (IllegalUpdateException | DatabaseException
+				| IdNotFoundException e) {
+			e.printStackTrace();
+			fail(e.getMessage());
+		}
+
+		try { // updating the id
+			V updated = database.update(id, new Database.Updater<V>() {
+				@Override
+				public V update(V value) {
+					assertValuesEqual(originalValue, value);
+					return createValue();
+				}
+			});
+			fail("The id has been updated: before = " + id + ", after = "
+					+ updated.getId());
+		} catch (DatabaseException e) {
+			e.printStackTrace();
+			fail(e.getMessage());
+		} catch (IllegalUpdateException e) {
+			// OK
+		}
+
+		try { // value to newValue -> normal updating
+			V updated = database.update(id, new Database.Updater<V>() {
+				@Override
+				public V update(V value) {
+					assertValuesEqual(originalValue, value);
+					return newValue;
+				}
+			});
+			assertValuesEqual(newValue, updated);
+			assertValuesEqual(newValue, database.get(id));
+		} catch (IllegalUpdateException | DatabaseException
+				| IdNotFoundException e) {
+			e.printStackTrace();
+			fail(e.getMessage());
+		}
+
+		try { // value to null -> remove
+			V updated = database.update(id, new Database.Updater<V>() {
+				@Override
+				public V update(V value) {
+					assertValuesEqual(newValue, value);
+					return null;
+				}
+			});
+			assertNull(updated);
+			assertFalse(database.exists(id));
+		} catch (IllegalUpdateException | DatabaseException e) {
 			e.printStackTrace();
 			fail(e.getMessage());
 		}
